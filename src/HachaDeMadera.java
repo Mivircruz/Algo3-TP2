54d3dccf6cc57d5a09cc1215ad195800c9431a66
Milagros Cruz <mivircruz@gmail.com>
2019-06-02 19:09:45 -0300

    Clases de todas las hachas y sus pruebas de inicializacion

---
+++ src/HachaDeMadera.java
@@ -0,0 +1,13 @@
+public class HachaDeMadera {
+
+    int durabilidad = 100;
+    int fuerza = 2;
+
+    public int getDurabilidad(){
+        return this.durabilidad;
+    }
+
+    public int getFuerza(){
+        return this.fuerza;
+    }
+}
---
+++ src/HachaDeMetal.java
@@ -0,0 +1,12 @@
+public class HachaDeMetal {
+    int durabilidad = 400;
+    int fuerza = 10;
+
+    public int getDurabilidad(){
+        return this.durabilidad;
+    }
+
+    public int getFuerza(){
+        return this.fuerza;
+    }
+}
