e128b7397edb4cade054bf283c6833f41a0b3a5e
Milagros Cruz <mivircruz@gmail.com>
2019-06-02 17:52:42 -0300

    Pruebas de hacha con sus clases creadas para que anden

---
+++ IdeaProjects/TP2Algo3/src/HachaDePiedra.java
@@ -0,0 +1,14 @@
+public class HachaDePiedra {
+    int durabilidad = 200;
+    int fuerza = 5;
+
+    public int getDurabilidad(){
+        return this.durabilidad;
+    }
+
+    public int getFuerza(){
+        return this.fuerza;
+    }
+}
+
+
