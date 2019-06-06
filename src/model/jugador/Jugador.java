caf70069ef3fe87081103c563ec52677c3af88e3
LuanCorrionero <38988939+LuanCorrionero@users.noreply.github.com>
2019-06-06 01:58:50 -0300

    Se agrega Jugador y sus pruebas

---
+++ src/model/jugador/Jugador.java
@@ -0,0 +1,18 @@
+package model.jugador;
+
+import model.herramientas.Hacha;
+import model.herramientas.Herramienta;
+import model.materiales.Madera;
+import model.materiales.Material;
+
+public class Jugador {
+
+    protected Herramienta herramientaEquipada = new Hacha(new Madera());
+
+    public Herramienta obtenerHerramientaEquipada() {
+        return herramientaEquipada;
+    }
+    public void usarHerramientaEquipadaSobre(Material material) {
+        herramientaEquipada.golpear(material);
+    }
+}
